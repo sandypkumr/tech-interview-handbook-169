80af0a73779114ddb4316d98457f85742f53b404
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2024-06-21 23:15:45 +0530

    Hit Counter

---
+++ src/queue/HitCounter.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+class HitCounter:
+    def __init__(self):
+        self.queue = deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.queue.append(timestamp)
+        while self.queue and self.queue[0] <= timestamp - 300:
+            self.queue.popleft()
+
+    def getHits(self, timestamp: int) -> int:
+        while self.queue and self.queue[0] <= timestamp - 300:
+            self.queue.popleft()
+        return len(self.queue)
